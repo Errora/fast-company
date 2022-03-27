5e33647ba23003446f11a8096085c4da16fcf9c4
Alla Khintal <feeramail@gmail.com>
2022-03-27 20:06:16 +0400

    Fix users

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -12,46 +12,63 @@ const Users = () => {
   }
 
   const renderUsers = () => {
-    return users.length !== 0 &&
-      users.map((user) => (
-        <tr key = {user}>
-          <td>{user.name}</td>
-          <td>{user.qualities}</td>
-          <td>{user.profession}</td>
-          <td>{user.completedMeetings}</td>
-          <td>{user.rate}</td>
-          <td
+    users.map((user) => (
+      <tr key = {user._id}>
+        <td>{user.name}</td>
+        <td>{user.qualities.map((item) => (
+          <span
+            className = {'badge m-1 bg-' + item.color}
+            key = {item._id}
+          >
+            {item.name}
+          </span>
+        ))}
+        </td>
+        <td>{user.profession}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}</td>
+        <td>
+          <button
             className = "btn btn-danger"
-            onClick = {() => handleDelete(user)}
+            // onClick = {() => handleDelete(user)}
           >
             Delete
-          </td>
-        </tr>
-      ))
-  }
-  if (users.length !== 0) {
-    return <ul>{renderUsers()}</ul>
+          </button>
+        </td>
+      </tr>
+    ))
   }
 
   return (
     <>
-      <table className = "table">
-        <thead>
-          <tr key = 'header'>
-            <th key = 'Имя' scope = "col">Имя</th>
-            <th key = 'Качества' scope = "col">Качества</th>
-            <th key = 'Профессия' scope = "col">Профессия</th>
-            <th key = 'Встретился' scope = "col">Встретился, раз</th>
-            <th key = 'Оценка' scope = "col">Оценка</th>
-            <th key = 'Кнопка' scope = "col"></th>
-          </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-      </table>
+      {users.length > 0 && (
+        <table className = "table">
+          <thead>
+            <tr key = 'header'>
+              <th key = 'Name' scope = "col">
+                Имя
+              </th>
+              <th key = 'Qualities' scope = "col">
+                Качества
+              </th>
+              <th key = 'Profession' scope = "col">
+                Профессия
+              </th>
+              <th key = 'CompletedMeetings' scope = "col">
+                Встретился, раз
+              </th>
+              <th key = 'Rate' scope = "col">
+                Оценка
+              </th>
+              <th key = 'Button' scope = "col">
+              </th>
+            </tr>
+          </thead>
+          <tbody>{renderUsers(users)}</tbody>
+        </table>
+      )}
     </>
-  );
+  )
 }
 
 export default Users;
